3e1bf4260201ce6cb292b6648f3f7d13cfd4351d
kuckjwi <rnrghks09@gmail.com>
2022-07-27 21:22:24 +0900

    feat: add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 	"streaming-grpc-exercise/configs"
 	"streaming-grpc-exercise/internal/database"
 	"streaming-grpc-exercise/internal/handler"
 	"streaming-grpc-exercise/pkg/repository"
 	"streaming-grpc-exercise/pkg/server"
 	"streaming-grpc-exercise/pkg/service"
+	"sync"
+	"syscall"
 )
 
 func main() {
@@ -25,9 +29,22 @@ func main() {
 	handler.RegisterBookService(grpcServer, container)
 	handler.RegisterHealthCheck(grpcServer)
 
+	wg := sync.WaitGroup{}
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	wg.Add(1)
+	go func() {
+		<-signalCh
+		grpcServer.GracefulStop()
+		wg.Done()
+		log.Println("Shutdown server...")
+	}()
+
 	log.Println("Starting server...")
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		panic(err)
 	}
+	wg.Wait()
 }
